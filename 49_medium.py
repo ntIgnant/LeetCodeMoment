4a6f582363e15c56b8bc8cf5d0683c8e7e87d34a
ignanttt <ignanttt@gmail.com>
2026-01-22 23:51:18 +0100

    UPDATED | problem 49 accepted

--- 49_medium.py
+++ 49_medium.py
@@ -5,12 +5,15 @@ print(anagrams_list)
 
 sorted_anagrams = ["".join(sorted(word)) for word in anagrams_list]
 print(sorted_anagrams)
-unique_anagrams = {hash(word) for word in sorted_anagrams}
-print(unique_anagrams)
+unique_anagrams = {}
 for anagram in anagrams_list:
     tmp_sorted = "".join(sorted(anagram))
     tmp_hashed = hash(tmp_sorted)
 
-    for key in unique_anagrams:
-        if tmp_hashed == key:
-            unique_anagrams.update(key, anagram)
+    if tmp_hashed in unique_anagrams:
+        unique_anagrams[tmp_hashed].append(anagram)
+    else:
+        unique_anagrams[tmp_hashed] = [anagram]
+
+
+print(list(unique_anagrams.values()))
